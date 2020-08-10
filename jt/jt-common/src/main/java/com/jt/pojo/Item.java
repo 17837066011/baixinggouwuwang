6bbf352c7076717b2da66d0d694e411d5cb1b79f
Administrator <Administrator@192.168.0.102>
2020-08-10 10:09:54 +0800

    01

--- jt/jt-common/src/main/java/com/jt/pojo/Item.java
+++ jt/jt-common/src/main/java/com/jt/pojo/Item.java
@@ -12,7 +12,7 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 public class Item extends BasePojo{
 	@TableId(type = IdType.AUTO)
-	private Long id;		    //定义主键
+	private Long id;		    //定义主  键
 	private String title;       //标题
 	private String sellPoint;	//卖点信息
 	private Long   price;		//商品价格 1.dubbo有精度问题 0.88888..+0.1111112=0.9999.... //2.速度  int > long > dubbo 将商品扩大100倍保存
